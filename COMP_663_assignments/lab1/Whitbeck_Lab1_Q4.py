9a7f47014140ec51993ce7d61ef7792838bafa9c
Kendall Whitbeck <kendallwhitbeck@gmail.com>
2024-09-15 16:32:42 -0700

    plotly.xexpress bar chart included but commented out

--- COMP_663_assignments/lab1/Whitbeck_Lab1_Q4.py
+++ COMP_663_assignments/lab1/Whitbeck_Lab1_Q4.py
@@ -9,21 +9,40 @@ COMP 663: Python for Data Science
 """
 
 import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 def main():
     # load the file titanic.csv into a data frame
-    titanic = pd.read_csv("titanic.csv")
+    titanic = pd.read_csv('titanic.csv')
 
     # subset the titanic dataset to include first class passengers who embarked in Southampton
-    first_south = titanic[(titanic.pclass == 1) & (titanic.embarked == "S")]
+    first_south = titanic[(titanic.pclass == 1) & (titanic.embarked == 'S')]
 
     # subset the titanic dataset to include either second or third class passengers
-    second_third_logOps = titanic[(titanic.pclass == 2) | (titanic.pclass == 3)]
     second_third = titanic[titanic.pclass.isin([2,3])]
 
+    # print the first five rows of both subsets
     print(first_south.head())
     print(second_third.head())
 
+    # create bar charts for the following:
+
+    # passengers in first class who embarked in Southampton grouped by sex
+    # fig_first_south_sex =px.bar(first_south, x='sex') # create bar chart
+    # fig_first_south_sex.show()  # show bar chart
+
+    first_south['sex'].value_counts().plot(kind='bar')
+    # plt.bar(first_south['sex'].value_counts().index, first_south['sex'].value_counts().values)
+    plt.xlabel('First')
+    plt.ylabel('Count')
+    plt.title('Passenger Class')
+    plt.xticks(rotation=0)
+    plt.grid(axis='y', which='major', linestyle='--', linewidth=0.5, zorder=0)
+    plt.show()
+
+    # passengers in second and third class grouped by survival status
+
 # ensure main() is executed only if this .py file is executed directly (i.e., not imported by another .py file)
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
